e1d05fe95a6997889bdd59e639e4f5dbbb228149
SaptarsiRoy <saptarsiroy12@gmail.com>
2021-10-12 19:05:49 +0530

    added constants file

---
+++ lib/constants.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var kSplashScreenTextStyle = TextStyle(
+  fontFamily: 'Ephesis',
+  fontSize: 100,
+  color: Colors.brown.shade800,
+);
+
+const kSignUpTextStyle = TextStyle(
+  fontSize: 20,
+  color: Colors.white,
+);
+
+var kButtonTextStyle = TextStyle(
+  fontSize: 17,
+  color: Colors.brown.shade800,
+);
+
+var kDetailsTextFieldStyle = GoogleFonts.lato(
+  fontWeight: FontWeight.w600,
+  color: Colors.grey.shade600,
+);
+
+const double kOldValue = 200;
+const double kNewValue = 80;
